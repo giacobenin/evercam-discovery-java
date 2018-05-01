700e6dfab2f19acf198e171e0d4104b8a8abbdb9
gbenincasa <gbenincasa@ihmc.us>
2018-05-01 01:19:37 -0400

    Added missing sources.

--- src/main/java/io/evercam/Auth.java
+++ src/main/java/io/evercam/Auth.java
@@ -1,7 +1,45 @@
 package io.evercam;
 
-/**
- * Created by gbeni on 5/1/2018.
- */
-public class Auth {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Auth
+{
+    private String type;
+    private JSONObject authJSONObject = null;
+    public static final String TYPE_BASIC = "basic";
+
+    protected Auth(String type, JSONObject authJSONObject)
+    {
+        this.type = type;
+        this.authJSONObject = authJSONObject;
+    }
+
+
+    public String getUsername() throws EvercamException
+    {
+        try
+        {
+            return authJSONObject.getString("username");
+        } catch (JSONException e)
+        {
+            return "";
+        }
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public String getPassword() throws EvercamException
+    {
+        try
+        {
+            return authJSONObject.getString("password");
+        } catch (JSONException e)
+        {
+            return "";
+        }
+    }
 }
--- src/main/java/io/evercam/EvercamException.java
+++ src/main/java/io/evercam/EvercamException.java
@@ -1,7 +1,34 @@
 package io.evercam;
 
-/**
- * Created by gbeni on 5/1/2018.
- */
-public class EvercamException {
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONException;
+
+import java.io.IOException;
+
+public class EvercamException extends Exception
+{
+    protected final static String MSG_USER_API_KEY_REQUIRED = "User API key and API ID required";
+    protected final static String MSG_INVALID_USER_KEY = "Invalid user api key/id";
+    protected final static String MSG_INVALID_AUTH = "Invalid auth";
+    protected final static String MSG_SERVER_ERROR = "Evercam internal server error.";
+
+    public EvercamException(String message)
+    {
+        super(message);
+    }
+
+    public EvercamException(UnirestException unirestException)
+    {
+        super(unirestException);
+    }
+
+    public EvercamException(JSONException jsonException)
+    {
+        super(jsonException);
+    }
+
+    public EvercamException(IOException ioException)
+    {
+        super(ioException);
+    }
 }
