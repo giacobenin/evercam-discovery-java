fa7ab6101a41cdc7dbfa4f7dc11923681896b5e1
gbenincasa <gbenincasa@ihmc.us>
2018-05-01 04:36:43 -0400

    Using common net.

--- src/main/java/io/evercam/network/discovery/ScanRange.java
+++ src/main/java/io/evercam/network/discovery/ScanRange.java
@@ -1,52 +1,33 @@
 package io.evercam.network.discovery;
 
+import org.apache.commons.net.util.SubnetUtils;
+
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import static io.evercam.network.discovery.IpTranslator.getUnsignedLongFromIp;
+
 public class ScanRange {
-    private long routerIp;
-    private String routerIpString;
-    private long scanStart;
-    private long scanEnd;
-    private int cidr;
+    private final long routerIp;
+    private final String routerIpString;
+    private final SubnetUtils utils;
 
     public ScanRange(String routerIp, String subnetMask) throws Exception {
-        this.routerIp = IpTranslator.getUnsignedLongFromIp(routerIp);
+        this.routerIp = getUnsignedLongFromIp(routerIp);
         this.routerIpString = routerIp;
-
-        cidr = IpTranslator.maskIpToCidr(subnetMask);
-        setUpStartAndEnd(cidr);
-    }
-
-    // TODO: Temporary disabled this because get cidr from interface is not
-    // working sometimes
-    // public ScanRange(NetworkInterface networkInterface) throws Exception
-    // {
-    // scanIp = IpTranslator.getUnsignedLongFromIp(NetworkInfo
-    // .getIpFromInterface(networkInterface));
-    // int cidr = NetworkInfo.getCidrFromInterface(networkInterface);
-    // setUpStartAndEnd(cidr);
-    // }
-
-    private void setUpStartAndEnd(int cidr) {
-        int shift = (32 - cidr);
-        if (cidr < 31) {
-            scanStart = (routerIp >> shift << shift) + 1;
-            scanEnd = (scanStart | ((1 << shift) - 1)) - 1;
-        } else {
-            scanStart = (routerIp >> shift << shift);
-            scanEnd = (scanStart | ((1 << shift) - 1));
-        }
+        this.utils = new SubnetUtils(routerIp, subnetMask);
     }
 
     /**
      * @return true if the given IP is in this scan range
      */
     public boolean containIp(String ip) throws Exception {
-        int shift = (32 - cidr);
-        long ipLong = IpTranslator.getUnsignedLongFromIp(ip);
-        return scanStart == ((ipLong >> shift << shift) + 1);
+        return utils.getInfo().isInRange(ip);
     }
 
-    public int size() {
-        return (int) (scanEnd - scanStart + 1);
+    public long size() {
+        return utils.getInfo().getAddressCountLong();
     }
 
     protected long getRouterIp() {
@@ -57,24 +38,20 @@ public class ScanRange {
         return routerIpString;
     }
 
+    public String toString() {
+        return new StringBuilder("[")
+                .append(utils.getInfo().getLowAddress())
+                .append(", ")
+                .append(utils.getInfo().getHighAddress())
+                .append("]")
+                .toString();
+    }
+
     protected long getScanStart() {
-        return scanStart;
+        return getUnsignedLongFromIp(utils.getInfo().getLowAddress());
     }
 
     protected long getScanEnd() {
-        return scanEnd;
+        return getUnsignedLongFromIp(utils.getInfo().getHighAddress());
     }
-
-    protected void setScanIp(long scanIp) {
-        this.routerIp = scanIp;
-    }
-
-    protected void setScanStart(long scanStart) {
-        this.scanStart = scanStart;
-    }
-
-    protected void setScanEnd(long scanEnd) {
-        this.scanEnd = scanEnd;
-    }
-
 }
